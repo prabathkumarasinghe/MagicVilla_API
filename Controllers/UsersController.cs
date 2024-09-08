179cd99a7e4c965207066b1a3ac2d55c41508759
pk <kpmprabath@gmail.com>
2024-09-08 13:57:28 +0930

    Create Token

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MagicVilla.Controllers
+{
+    public class UsersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- Repository/IRepository/IUserRepository.cs
+++ Repository/IRepository/IUserRepository.cs
@@ -8,7 +8,7 @@ namespace MagicVilla.Repository.IRepository
     public interface IUserRepository
     {
         bool IsUniqueUser(string username);
-        Task<LoginResponseDto> Login(LoginResponseDto loginResponseDto);
+        Task<LoginResponseDto> Login(LoginRequestDto loginRequestDto);
         Task<LocalUser> Register(RegisterationRequestDto registerationRequestDto);
     }
 }
